27a30b171fac95ba8a4fb61a443a7aa0289e4e46
deepm24 <deep60640@gmail.com>
2023-02-15 18:20:07 +0530

    Added:UC3-Calculate Daily Wage & halfday wage

--- Attendance.java
+++ Attendance.java
@@ -7,16 +7,22 @@ public class Attendance
     public void attendance()
     {
         int perhour=20;
-        int dayhour=8;
+        int fulldayhour=8;
+        int halfdayhour=4;
         Random obj = new Random();
-        int check = obj.nextInt(2);
+        int check = obj.nextInt(3);
         if (check == 0)
         {
             System.out.println("Absent\nDay Wage is Zero");
         }
+        else if(check==1)
+        {
+            System.out.println("Employee present for half day\n"+"Employee half Day Wage is:"+perhour*halfdayhour);
+
+        }
         else
         {
-            System.out.println("Employee present\n"+"Employee Day Wage is:"+perhour*dayhour);
+            System.out.println("Employee present\n"+"Employee Day Wage is:"+perhour*fulldayhour);
         }
     }
 }
--- Main.java
+++ Main.java
@@ -1,12 +1,15 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Welcome to Employee Wage Computation");
+        System.out.println("Welcome to Employee Wage Computation\n");
 
         Attendance obj=new Attendance();
         obj.attendance();
+
     }
 }
