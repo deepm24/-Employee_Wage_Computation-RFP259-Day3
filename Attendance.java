0afb8953510df6ef4a4a0567406848e9bb4078a6
deepm24 <deep60640@gmail.com>
2023-02-15 18:30:04 +0530

    Added:UC4-Calculate Daily Wage & halfday wage By Switch Case

--- Attendance.java
+++ Attendance.java
@@ -11,18 +11,18 @@ public class Attendance
         int halfdayhour=4;
         Random obj = new Random();
         int check = obj.nextInt(3);
-        if (check == 0)
-        {
-            System.out.println("Absent\nDay Wage is Zero");
-        }
-        else if(check==1)
-        {
-            System.out.println("Employee present for half day\n"+"Employee half Day Wage is:"+perhour*halfdayhour);
 
-        }
-        else
+        switch (check )
         {
+            case 0:
+            System.out.println("Absent\nDay Wage is Zero");
+            break;
+            case 1:
+            System.out.println("Employee present for half day\n"+"Employee half Day Wage is:"+perhour*halfdayhour);
+            break;
+            case 2:
             System.out.println("Employee present\n"+"Employee Day Wage is:"+perhour*fulldayhour);
+            break;
         }
     }
 }
