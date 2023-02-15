c81e2571e32688074ed092168caae3268ae44321
deepm24 <deep60640@gmail.com>
2023-02-15 17:47:47 +0530

    Added:UC2-Calculate Daily Wage

--- Attendance.java
+++ Attendance.java
@@ -2,17 +2,21 @@ package org.example;
 
 import java.util.Random;
 
-public class Attendance {
-    public void attendance() {
+public class Attendance
+{
+    public void attendance()
+    {
+        int perhour=20;
+        int dayhour=8;
         Random obj = new Random();
         int check = obj.nextInt(2);
         if (check == 0)
         {
-            System.out.println("Absent");
+            System.out.println("Absent\nDay Wage is Zero");
         }
         else
         {
-            System.out.println("present");
+            System.out.println("Employee present\n"+"Employee Day Wage is:"+perhour*dayhour);
         }
     }
 }
